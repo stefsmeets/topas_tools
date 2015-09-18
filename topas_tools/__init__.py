cb7d3ecb4d19e995fb91dce4e14cd2014e1ab311
Stef Smeets <stef.smeets@mat.ethz.ch>
2015-09-18 18:09:38 +0200

    Added readme

---
+++ topas_tools/__init__.py
@@ -0,0 +1,23 @@
+# Topas tools
+
+## Requirements
+
+- Python2.7
+- numpy
+- matplotlib
+- pandas
+- CCTBX
+
+## Installation
+
+Download and extract:
+
+https://github.com/stefsmeets/topas_tools/archive/master.zip
+
+Install:
+
+    python setup.py install
+
+Uninstall:
+
+    pip uninstall topas_tools
