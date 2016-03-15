1d28574e912826ad3d30b6802c348cd54b52b0d8
Stef Smeets <stef.smeets@mmk.su.se>
2016-03-15 10:15:42 +0100

    Fix bugs and clean up output

--- topas_tools/cif2topas.py
+++ topas_tools/cif2topas.py
@@ -63,7 +63,7 @@ def main():
         exit()
 
     for cif in cifs:
-        print 'Reading file: {}'.format(cif)
+        # print 'Reading file: {}'.format(cif)
         structures = read_cif(cif)
         for name in structures:
             structure = structures[name].sites_mod_positive()
@@ -73,50 +73,50 @@ def main():
             scatterers = structure.scatterers()
 
             print """
-        str
-            Out_X_Yobs(x_yobs.xy)
-            Out_X_Ycalc(x_ycalc.xy)
-            Out_X_Difference(x_ydiff.xy)
-            Create_2Th_Ip_file(ticks.out)
+   str
+      Out_X_Yobs(x_yobs.xy)
+      Out_X_Ycalc(x_ycalc.xy)
+      Out_X_Difference(x_ydiff.xy)
+      Create_2Th_Ip_file(ticks.out)
 
-            Out_fobs(fobs.hkl)
+      Out_fobs(fobs.hkl)
 
-            Out_CIF_STR(structure.cif)"""
+      Out_CIF_STR(structure.cif)"""
 
             print """       
-            scale  @  1.0
-            r_bragg  1.0"""
+      scale  @  1.0
+      r_bragg  1.0"""
             print
 
-            s400 = "        'prm s400 1.0 \n"
-            s040 = "        'prm s040 1.0 \n"
-            s004 = "        'prm s004 1.0 \n"
-            s220 = "        'prm s220 1.0 \n"
-            s202 = "        'prm s202 1.0 \n"
-            s022 = "        'prm s022 1.0 \n"
-            s301 = "        'prm s301 1.0 \n"
-            s121 = "        'prm s121 1.0 \n"
-            s103 = "        'prm s103 1.0 \n"
-            eta = "        'prm !eta 0.5 min 0.0 max 1.0\n"
+            s400 = "      'prm s400 1.0 \n"
+            s040 = "      'prm s040 1.0 \n"
+            s004 = "      'prm s004 1.0 \n"
+            s220 = "      'prm s220 1.0 \n"
+            s202 = "      'prm s202 1.0 \n"
+            s022 = "      'prm s022 1.0 \n"
+            s301 = "      'prm s301 1.0 \n"
+            s121 = "      'prm s121 1.0 \n"
+            s103 = "      'prm s103 1.0 \n"
+            eta  = "      'prm !eta 0.5 min 0.0 max 1.0\n"
             if sg.crystal_system() == 'Monoclinic':
                 string = "{s400}{s040}{s004}{s220}{s202}{s022}{s301}{s121}{s103}{eta}".format(
                     s400=s400, s040=s040, s004=s004, s220=s220, s202=s202, s022=s022, s301=s301, s121=s121, s103=s103, eta=eta)
-                macro = "       'Stephens_monoclinic(s400, s040, s004, s220, s202, s022, s301, s121, s103, eta)"
+                macro = "      'Stephens_monoclinic(s400, s040, s004, s220, s202, s022, s301, s121, s103, eta)"
 
             elif sg.crystal_system() == 'Tetragonal':
                 string = "{s400}{s004}{s220}{s202}{eta}".format(
                     s400=s400, s040=s040, s004=s004, s220=s220, s202=s202, s022=s022, s301=s301, s121=s121, s103=s103, eta=eta)
-                macro = "       'Stephens_tetragonal(s400, s004, s220, s202, eta)"
+                macro = "      'Stephens_tetragonal(s400, s004, s220, s202, eta)"
 
             elif sg.crystal_system() == 'Hexagonal':
                 string = "{s400}{s004}{s202}{eta}".format(
                     s400=s400, s040=s040, s004=s004, s220=s220, s202=s202, s022=s022, s301=s301, s121=s121, s103=s103, eta=eta)
-                macro = "       'Stephens_hexagonal(s400, s202, s004, eta)"
+                macro = "      'Stephens_hexagonal(s400, s202, s004, eta)"
 
             elif sg.crystal_system() == 'Orthorhombic':
                 string = "{s400}{s040}{s004}{s220}{s202}{s022}{eta}".format(
                     s400=s400, s040=s040, s004=s004, s220=s220, s202=s202, s022=s022, s301=s301, s121=s121, s103=s103, eta=eta)
-                macro = "       'Stephens_orthorhombic(s400, s040, s004, s220, s202, s022, eta)"
+                macro = "      'Stephens_orthorhombic(s400, s040, s004, s220, s202, s022, eta)"
             else:
                 string = ""
                 macro = ""
@@ -124,25 +124,25 @@ def main():
             print macro
 
             print """
-            PV_Peak_Type(
-            ha,    0.02,
-            !hb,   0.0,
-            !hc,   0.0,
-            lora,  0.5,
-            !lorb, 0.0,
-            !lorc, 0.0)
+      PV_Peak_Type(
+      ha,    0.02,
+      !hb,   0.0,
+      !hc,   0.0,
+      lora,  0.5,
+      !lorb, 0.0,
+      !lorc, 0.0)
 
-            'Simple_Axial_Model(axial, 0.0)
+      'Simple_Axial_Model(axial, 0.0)
 
-            Phase_Density_g_on_cm3( 1.0)"""
+      Phase_Density_g_on_cm3( 1.0)"""
 
             print """
-            view_structure
+      view_structure
 
-            'fourier_map 1
-            '   fourier_map_formula = Fobs - Fcalc;"""
+      'fourier_map 1
+      '   fourier_map_formula = Fobs - Fcalc;"""
             print
-            print '     space_group "{}"'.format(sg.type().universal_hermann_mauguin_symbol().replace(" ", ""))
+            print '      space_group "{}"'.format(sg.type().universal_hermann_mauguin_symbol().replace(" ", ""))
             print
             a, b, c, al, be, ga = uc.parameters()
             if sg.crystal_system() == 'Cubic':
@@ -156,23 +156,22 @@ def main():
 
             elif sg.crystal_system() == 'Rhombohedral':
                 string = "      Rhombohedral(@ {a}, @ {al})"
-
             else:
-                string = """        a  @  {a:8.5f}
-            b  @  {b:8.5f}
-            c  @  {c:8.5f}
-            al {refal:1}  {al}
-            be {refbe:1}  {be}
-            ga {refga:1}  {ga}"""
+                string = """      a  @  {a:8.5f}
+      b  @  {b:8.5f}
+      c  @  {c:8.5f}
+      al {refal:1}  {al}
+      be {refbe:1}  {be}
+      ga {refga:1}  {ga}"""
             refal = "" if al == 90 else "@"
             refbe = "" if be == 90 else "@"
             refga = "" if ga == 90 else "@"
             print string.format(a=a, b=b, c=c, refal=refal, al=al, refbe=refbe, be=be, refga=refga, ga=ga)
             print
-            print "     volume {:.2f}".format(uc.volume())
+            print "      volume {:.2f}".format(uc.volume())
             print
             for element in set(scatterers.extract_scattering_types()):
-                print "     prm beq{element:2s}  2.0  min 1.0  max 5.0".format(element=element)
+                print "      prm beq{element:2s}  2.0  min 1.0  max 5.0".format(element=element)
             print
 
             z_order = sg.order_z()
@@ -184,10 +183,10 @@ def main():
                 element = atom.element_symbol()
                 mult = atom.multiplicity()
 
-                print "     site {label:5s}  num_posns {mult:3d}  x  {x:.5f}  y  {y:.5f}  z  {z:.5f}  occ {element:2s}  1.0  beq = beq{element:2s};".format(label=label, mult=mult, x=x, y=y, z=z, element=element),
+                print "      site {label:5s}  num_posns {mult:3d}  x  {x:.5f}  y  {y:.5f}  z  {z:.5f}  occ {element:2s}  1.0  beq =beq{element:2s};".format(label=label, mult=mult, x=x, y=y, z=z, element=element),
 
                 if mult < z_order:
-                    print "        ' {:6s} {:6s} {:6s}".format(*sps.site_symmetry(atom.site).special_op_simplified().terms)
+                    print "   ' {:5s} {:5s} {:5s}".format(*sps.site_symmetry(atom.site).special_op_simplified().terms)
                 else:
                     print
 
